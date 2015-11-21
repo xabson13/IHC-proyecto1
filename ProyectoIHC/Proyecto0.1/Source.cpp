8e9d649b8c9733c4706b1f3d2900a09dc1ff6837
angel208 <angel_fp90@hotmail.com>
2015-11-20 21:04:39 -0430

    Primer Commit Comentado

--- ProyectoIHC/Proyecto0.1/Source.cpp
+++ ProyectoIHC/Proyecto0.1/Source.cpp
@@ -11,7 +11,7 @@
 
 using namespace cv;
 
-//filtros del threshold
+//valores iniciales de HSV para detectar la lineas (color verde)
 int H_MIN = 42;
 int H_MAX = 93;
 int S_MIN = 77;
@@ -44,17 +44,18 @@ int main(int argc, char* argv[])
 	Mat CannyThresh;
 	
 
-	//video capture object to acquire webcam feed
+	//objeto VideoCapture que se usara para objener la imagen de la camara
 	cv::VideoCapture capture;
 
-	//open capture object at location zero (default location for webcam)
+	//prende la camara
 	capture.open(0);
-	//set height and width of capture frame
+	//dimensiones de la ventana que se usara para mostrar las imagenes.
 	capture.set(CV_CAP_PROP_FRAME_WIDTH, 640);
 	capture.set(CV_CAP_PROP_FRAME_HEIGHT, 480);
-	//start an infinite loop where webcam feed is copied to cameraFeed matrix
-	//all of our operations will be performed within this loop
 
+	//Empieza el bucle principal en el que se realizaran todas la operaciones.
+	//en este loop se obtiene la imagen (frama) actual de la camara, se procesa, y luego se muestra el resultado final. esto se hace varias veces haciendo que 
+	//el procesamiento parezca en tiempo real.
 	while (true){ 
 		//lee del objeto capture (camara) y almacena la imagen en input
 		capture.read(input);
@@ -62,32 +63,35 @@ int main(int argc, char* argv[])
 		cvtColor(input, HSV, COLOR_BGR2HSV);
 		//aplica el filtro Threshold con los valores hsv minimos y maximos y lo almacena en una matris "threshold"
 		inRange(HSV, Scalar(H_MIN, S_MIN, V_MIN), Scalar(H_MAX, S_MAX, V_MAX), threshold);
-		//aplica las morfologias a la imagen
+		//aplica las morfologias a la imagen (leer acerca de las operaciones morfologicas en procesamiento de imagenes)
 		ApicarMorfologia(threshold);
 		//aplicar canny edge
 		Canny(threshold, CannyThresh, 100, 50);
 		//aplicar houghline
-		// detect lines
+		// detecta las lineas y mete la informacion en el vector lines
 		std::vector<Vec2f> lines;
 		HoughLines(CannyThresh, lines, 1, CV_PI/180, 100 );
 
 
-		// draw lines 
+		//Obtiene los puntos usando la informacion devuelta por HoughLine() y dibuja cada una de las lineas detectadas.
+		//las lineas se dibujan obteniendo dos puntos y metiendolos en la opeeracion line().
+		//debido a q tenemos dos puntos de cada linea, estos se pueden usar para el momento de verificar la posicion de las notas con respecto a las lineas.
 	    for( size_t i = 0; i < lines.size(); i++ )
 		{
 			float rho = lines[i][0], theta = lines[i][1];
 			Point pt1, pt2;
 			double a = cos(theta), b = sin(theta);
 			double x0 = a*rho, y0 = b*rho;
+			//punto 1 de la recta actual
 			pt1.x = cvRound(x0 + 1000*(-b));
 			pt1.y = cvRound(y0 + 1000*(a));
+			//punto 2 de la recta actual
 			pt2.x = cvRound(x0 - 1000*(-b));
 			pt2.y = cvRound(y0 - 1000*(a));
 			line(input, pt1, pt2, Scalar(0, 0, 255), 3, CV_AA);
 		} 
 		
 		//muestra en una ventana llamada "input" lo que esta contenido en la matriz input.
-		//imshow("camera", CannyThresh); 
 		imshow("camera", input);
 
 
